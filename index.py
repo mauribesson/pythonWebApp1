2802de0f487c17451ca5f6d49cd2f55a7db7a185
mauribesson <mauribesson@gmail.com>
2019-05-25 17:41:56 -0300

    Seccionde de Blog

--- index.py
+++ index.py
@@ -10,5 +10,9 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
